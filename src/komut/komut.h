847fc98a4fcd9b8efd2628cb88e6f9dee53cff9d
hdinceler <hdinceler@gmail.com>
2026-02-14 15:24:49 +0300

    Auto commit: 2026-02-14 15:24:49

--- src/komut/komut.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <Arduino.h>
-void komut_ayaga_kaldir();
-uint16_t komut_oku();
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,8 @@
-#include "komut/komut.h"
+#include <Arduino.h>
+
 
 void setup() {
-  komut_ayaga_kaldir();
-  // mqtt_ayaga_kaldir()
+
 }
 
-void loop() {
-  uint16_t komut = komut_oku();
-  if (komut != 0) {
-    Serial.print("Komut alındı: ");
-    Serial.println(komut);
-    // Komutu işleme kodu buraya gelecek
-  }
-}
- 
+void loop(){}
